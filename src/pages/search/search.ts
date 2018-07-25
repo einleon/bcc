296b290f57019fd678b0d4d196fe4b303524f901
keinleon <blubb.leon@gmail.com>
2018-07-25 15:40:05 +0200

    Search-Test

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -15,8 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
+  searchQuery: string = '';
+  items: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
   }
+
+  initializeItems() {
+    this.items = [
+      'Hubertus Salzgeberus',
+      'Stephan Klaiber',
+      'SkanBahl',
+      'Perl Scheter'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
