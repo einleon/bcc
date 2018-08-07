16ccd8d37f8da10908af03e6657ea1af459f0331
keinleon <blubb.leon@gmail.com>
2018-08-07 17:40:50 +0200

    Profil-Tab dynamisch (holt nun Daten aus erstellten Array bei Typescript)

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -16,11 +16,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ProfilePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.experience_data = [
+      { year: "2018", job: "BA at MCI" },
+      { year: "2017", job: "Hilfsarbeiter McDonalds" },
+      { year: "2016", job: "Müllmann" },
+      { year: "2015", job: "Arbeitslos" }
+    ];
+
+    this.skill_data = [
+      { number: "1", skill: "Web-development" },
+      { number: "2", skill: "Agile project manager" },
+      { number: "3", skill: "Java Developer" },
+    ];
+
+    this.skillCollection = [];
+    this.skillCollection.push(this.skill_data);
+    this.skilldata = this.skillCollection[Math.floor((Math.random() * this.skillCollection.length))];
+    console.log(this.skilldata);
+
+
+    this.experienceCollection = [];
+    this.experienceCollection.push(this.experience_data);
+    this.experiencedata = this.experienceCollection[Math.floor((Math.random() * this.experienceCollection.length))];
+    console.log(this.experiencedata);
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
   }
-
 
 }
