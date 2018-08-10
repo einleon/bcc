d6731a467e84a933ee252f324262b368dd423a13
keinleon <blubb.leon@gmail.com>
2018-08-10 14:18:34 +0200

    Profiltab: Daten werden aus JSON file angezeigt (ausgenommen Experience Bereich!)

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,101 +25,7 @@ export class ProfilePage {
   place: any;
   design_card: any;
 
-
-/*
-  all_data: any;
-  all_dataCollection: any;
-  alldata: any;
-
-  userData: any;
-  contactData: any;
-  experienceData: any;
-  skillData: any;
-  contactDataCollection: any;
-  userDataCollection: any;
-  experienceCollection: any;
-  skillCollection: any;
-  userLog: any;
-  contactLog: any;
-  experienceLog: any;
-  skillLog: any;
-
-*/
-
   constructor(public navCtrl: NavController, public navParams: NavParams, private dataFinder : DataFinder) {
-
-    /*
-    this.userData = [
-      {slogan: "Young - motivated - dynamic", job: "CEO", company: "Kölbö"},
-      {firstname: "Perl", lastname: "Scheter"},
-      {design_card: "error404"}
-    ]
-
-    this.contactData = [
-      {phone: "+43 680 12345678"},
-      {address: "Radezkystraße 2"},
-      {place: "Lustenau"},
-      {postcode: "6890"},
-      {mail: "ichwillbier@gmail.com"}
-    ]
-
-    this.experienceData = [
-      {year: "2018", done_job: "BA at MCI"},
-      {year: "2017", done_job: "Hilfsarbeiter McDonalds"},
-      {year: "2016", done_job: "Müllmann"},
-      {year: "2015", done_job: "Arbeitslos"}
-    ]
-
-    this.skillData = [
-      {skill: "Web-development"},
-      {skill: "Agile project manager"},
-      {skill: "Java Developer"},
-    ]
-
-    this.all_data = [
-      {firstname: "Perl", lastname: "Scheter"},
-      {mail: "ichwillbier@gmail.com"},
-      {slogan: "Young - motivated - dynamic"},
-      {job: "CEO", company: "Kölbö"},
-      {year: "2018", job: "BA at MCI"},
-      {year: "2017", job: "Hilfsarbeiter McDonalds"},
-      {year: "2016", job: "Müllmann"},
-      {year: "2015", job: "Arbeitslos"},
-      {topskill1: "Web-development"},
-      {topskill2: "Agile project manager"},
-      {topskill3: "Java Developer"},
-      {phone: "+43 680 12345678"},
-      {address: "Radezkystraße 2"},
-      {place: "Lustenau"},
-      {design_card: "error404"}
-    ]
-
-    this.all_dataCollection = [];
-    this.all_dataCollection.push(this.all_data);
-    this.alldata = this.all_dataCollection[Math.floor((Math.random() * this.all_dataCollection.length))];
-    console.log(this.alldata);
-
-    this.userDataCollection = [];
-    this.userDataCollection.push(this.userData);
-    this.userLog = this.userDataCollection[Math.floor((Math.random() * this.userDataCollection.length))];
-    console.log(this.userLog);
-
-    this.experienceCollection = [];
-    this.experienceCollection.push(this.experienceData);
-    this.experienceLog = this.experienceCollection[Math.floor((Math.random() * this.experienceCollection.length))];
-    console.log(this.experienceLog);
-
-    this.skillCollection = [];
-    this.skillCollection.push(this.skillData);
-    this.skillLog = this.skillCollection[Math.floor((Math.random() * this.skillCollection.length))];
-    console.log(this.skillLog);
-
-    this.contactDataCollection = [];
-    this.contactDataCollection.push(this.contactData);
-    this.contactLog = this.contactDataCollection[Math.floor((Math.random() * this.contactDataCollection.length))];
-    console.log(this.contactLog);
-    */
-
   }
 
   ionViewDidLoad() {
@@ -147,8 +53,10 @@ export class ProfilePage {
     this.place= data.place;
     this.design_card= data.design_card;
 
-    console.log(this.firstname);
     console.log(this.experience);
+    console.log(this.experience.length);
+
+
   }
 
 }
